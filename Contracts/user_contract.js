3fd5cc14102954efef819212e88f6ac682a8b894
devgru-3-2 <bharath.ambrish@gmail.com>
2023-04-25 01:39:47 +0530

    init-2

---
+++ Contracts/user_contract.js
@@ -0,0 +1,9 @@
+// User_Contract.js
+
+module.exports = {
+    contractABI: [
+      // your contract ABI
+    ],
+    contractAddress: '0x1234567890abcdef...' // replace with your actual contract address
+  };
+  
--- express_app/app.js
+++ express_app/app.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const Web3 = require('web3');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const { contractABI, contractAddress } = require('./Contracts/user_contract.js');
 
 const app = express();
 
-const MyContractABI = require('./Contracts/MyContractABI.json');
-const contractAddress = '0x1234567890abcdef...'; // replace with your actual contract address
+// Load environment variables from .env file
+dotenv.config();
 
-const infuraProjectId = process.env.INFURA_PROJECT_ID;
+// Connect to MongoDB database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-
-const infuraEndpoint = `https://goerli.infura.io/v3/${infuraProjectId}`;
+const infuraEndpoint = `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
 const web3 = new Web3(new Web3.providers.HttpProvider(infuraEndpoint));
 
+const myContract = new web3.eth.Contract(contractABI, contractAddress);
 
-const myContract = new web3.eth.Contract(MyContractABI, contractAddress);
-
-
+// Middleware to make the contract instance available to all routes
+app.use((req, res, next) => {
+  req.myContract = myContract;
+  next();
+});
 
 const userRouter = require('./routers/userRouter');
 app.use('/users', userRouter);
 
-const authRouter = require('./express_app/routers/auth');
-app.use('/auth', authRouter);
+// Serve static files from the public folder
+app.use(express.static('public'));
 
+// Set up your routes and middleware here
 
-
-
-// set up your routes and middleware here
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
